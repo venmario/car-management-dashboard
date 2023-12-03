be51ba1f82d05a3be17054f58f63a7d1a57c4ccf
venmario <glacius2ndmioogg@gmail.com>
2023-12-03 13:22:25 +0700

    feat: remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./assets/css/index.css";
 import router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
